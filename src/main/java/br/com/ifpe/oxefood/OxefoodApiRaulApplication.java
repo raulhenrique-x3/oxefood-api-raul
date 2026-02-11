0f0d8591180aed073b6648c918a764b138e347b3
Raul Henrique <raulheri43@hotmail.com>
2026-02-11 20:13:15 -0300

    feat:java-config

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiRaulApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiRaulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiRaulApplication.class, args);
+	}
+
+}
